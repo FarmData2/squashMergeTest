2ea9c406ccb8171d1cadbbedcbf0441bfdc2c664
Ty Chermsirivatana <chermsit@dickinson.edu>
2025-07-28 14:46:58 -0400

    Update run-tests.sh

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -2,6 +2,4 @@
 
 # Quick script to run bats tests
 # Load bats-support and bats-assert
-load 'test_helper/bats-support/load'
-load 'test_helper/bats-assert/load'
 ./bats/bin/bats "$@" test_squashMergePR.bats
